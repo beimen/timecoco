583fc0365cfe49dfe556b2ba7df0d11530616c58
beimenjun <beimenjun@gmail.com>
2015-03-10 21:57:32 +0800

    初始化项目。

---
+++ timecoco/timecoco/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  timecoco
+//
+//  Created by Hong Xie on 9/3/15.
+//  Copyright (c) 2015 timecoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ timecoco/timecoco/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  timecoco
+//
+//  Created by Hong Xie on 9/3/15.
+//  Copyright (c) 2015 timecoco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

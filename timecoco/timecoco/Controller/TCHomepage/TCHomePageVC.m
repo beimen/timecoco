e439021b030fbe307fe8279e9c79a2f37c5dbc52
beimen <beimenjun@gmail.com>
2015-03-14 23:33:29 +0800

    规范了命名 增加了一个TCColorManager，用于管理颜色

--- timecoco/timecoco/Controller/TCHomepage/TCHomePageVC.m
+++ timecoco/timecoco/Controller/TCHomepage/TCHomePageVC.m
@@ -1,20 +1,20 @@
 //
-//  ViewController.m
+//  TCHomepageVC.m
 //  timecoco
 //
 //  Created by Hong Xie on 9/3/15.
 //  Copyright (c) 2015 timecoco. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "TCHomepageVC.h"
 
-@interface ViewController ()
+@interface TCHomepageVC ()
 
 @property (nonatomic, strong) UITableView *tableView;
 
 @end
 
-@implementation ViewController
+@implementation TCHomepageVC
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
---
+++ timecoco/timecoco/Controller/TCHomepage/TCHomepageVC.h
@@ -0,0 +1,15 @@
+//
+//  TCHomepageVC.h
+//  timecoco
+//
+//  Created by Hong Xie on 9/3/15.
+//  Copyright (c) 2015 timecoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCHomepageVC : UIViewController
+
+
+@end
+

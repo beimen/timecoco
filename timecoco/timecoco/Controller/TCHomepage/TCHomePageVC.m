afe2b65a487de0eca7a56bdd7bc725d446ce328d
beimen <beimenjun@gmail.com>
2015-03-15 10:20:08 +0800

    添加素材

--- timecoco/timecoco/Controller/TCHomepage/TCHomePageVC.m
+++ timecoco/timecoco/Controller/TCHomepage/TCHomePageVC.m
@@ -10,8 +10,6 @@
 
 @interface TCHomepageVC ()
 
-@property (nonatomic, strong) UITableView *tableView;
-
 @end
 
 @implementation TCHomepageVC
@@ -33,6 +31,25 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuseIdentifier" forIndexPath:indexPath];
+    // Configure the cell...
+    
+    return cell;
 }
 
 @end

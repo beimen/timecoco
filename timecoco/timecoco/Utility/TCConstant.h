2bbadcc3d8b42385f073e44616e8ecddb05f04e0
beimenjun <beimenjun@gmail.com>
2015-03-18 23:18:49 +0800

    新增两个头文件，用于存储常量和通知的宏

---
+++ timecoco/timecoco/Utility/TCConstant.h
@@ -0,0 +1,13 @@
+//
+//  TCConstant.h
+//  timecoco
+//
+//  Created by Xie Hong on 3/18/15.
+//  Copyright (c) 2015 timecoco. All rights reserved.
+//
+
+#ifndef timecoco_TCConstant_h
+#define timecoco_TCConstant_h
+
+
+#endif

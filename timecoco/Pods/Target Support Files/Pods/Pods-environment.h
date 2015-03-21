36beb458f82be6305c9ff5c6ac298c2553c4a0f0
beimenjun <beimenjun@gmail.com>
2015-03-21 09:28:28 +0800

    新增一个pod库

--- timecoco/Pods/Target Support Files/Pods/Pods-environment.h
+++ timecoco/Pods/Target Support Files/Pods/Pods-environment.h
@@ -24,3 +24,9 @@
 #define COCOAPODS_VERSION_MINOR_FMDB_standard 5
 #define COCOAPODS_VERSION_PATCH_FMDB_standard 0
 
+// REFrostedViewController
+#define COCOAPODS_POD_AVAILABLE_REFrostedViewController
+#define COCOAPODS_VERSION_MAJOR_REFrostedViewController 2
+#define COCOAPODS_VERSION_MINOR_REFrostedViewController 4
+#define COCOAPODS_VERSION_PATCH_REFrostedViewController 7
+

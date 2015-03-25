ef6537f09904f79650873a80d297f5f0d69620af
beimenjun <beimenjun@gmail.com>
2015-03-25 22:48:23 +0800

    编辑调整时间至0.7秒。时间轴点击小时数可以显示分钟数。

--- timecoco/timecoco/Controller/TCMenu/TCMenuVC.m
+++ timecoco/timecoco/Controller/TCMenu/TCMenuVC.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.tableView.backgroundColor = TC_BACK_COLOR;
+    self.tableView.backgroundColor = TC_CLEAR_COLOR;
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
 }
 
